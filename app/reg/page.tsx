0288a2a229a4b8f6d7fb69af206411703c7736d1
Ethan Clarke <eclarkelackagh@gmail.com>
2025-07-11 16:45:41 +0100

    Added Day Ticket type options

--- app/reg/page.tsx
+++ app/reg/page.tsx
@@ -2,21 +2,73 @@
 
 import { AuthWrapper } from '../components/authWrapper'
 
-import { handleCheckout } from '../utils/stripeCheckoutUtil';
 import { useAuth } from '../context/authContext';
 import Loading from '../components/loading';
+import Link from 'next/link';
 
 export default function UserDetailsPage() {
     const { user } = useAuth()
 
+    const SATURDAY = "SATURDAY"
+    const SUNDAY = "SUNDAY"
+    const WEEKEND = "WEEKEND"
+    const STEP_TWO = "/reg/ticket-tier"
+
     if (!user) { return <Loading /> }
+
+    let currentStep = 0
+
+    function getStepStatus(stepElement: number): string {
+        return currentStep >= stepElement
+            ? "step step-primary"
+            : "step"
+    }
 
     return (
         <AuthWrapper>
-            <h2 className='font-[family-name:var(--font-sora)] text-xl'>
-                Choose a ticket type
-            </h2>
-            <button className='btn' onClick={() => handleCheckout(user.id, 'price_1RiKMxQslLjui9q05yaSXxx5')}>Standard Weekend Ticket</button>
+            <ul className="steps">
+                <li className={getStepStatus(0)}>Attendance</li>
+                <li className={getStepStatus(1)}>Ticket Tier</li>
+                <li className={getStepStatus(2)}>Personal Info</li>
+                <li className={getStepStatus(3)}>Payment</li>
+                <li className={getStepStatus(4)}>Confirmation</li>
+            </ul>
+
+            <div className="carousel w-full">
+                <div id="step1" className="carousel-item relative w-full">
+                    <span className='font-[family-name:var(--font-sora)] text-xl'>
+                        Choose your Attendance Days
+                    </span>
+
+                    <Link href={{
+                        pathname: STEP_TWO,
+                        query: SATURDAY
+                    }} className='btn'>
+                        Day Pass: Saturday
+                    </Link>
+
+                    <Link href={{
+                        pathname: STEP_TWO,
+                        query: SUNDAY
+                    }} className='btn'>
+                        Day Pass: Sunday
+                    </Link>
+
+                    <Link href={{
+                        pathname: STEP_TWO,
+                        query: WEEKEND
+                    }} className='btn'>
+                        Full Weekend
+                    </Link>
+                </div>
+
+                <div id="step2" className="carousel-item relative w-full">
+                    <span>Step 2</span>
+                </div>
+            </div>
+
+            {/* <button className='btn' onClick={() => handleCheckout(user.id, 'price_1RiKMxQslLjui9q05yaSXxx5')}>Full Weekend</button> */}
+
         </AuthWrapper >
     )
 }
