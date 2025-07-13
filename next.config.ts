d8958e497e12763a4fae62ca3cc07060a86ee244
Ethan Clarke <eclarkelackagh@gmail.com>
2025-07-13 14:58:17 +0100

    Updated /reg to redirect to /reg/select-day

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/reg',
+        destination: '/reg/select-day',
+        permanent: true,
+      }
+    ]
+  }
 };
 
 export default nextConfig;
