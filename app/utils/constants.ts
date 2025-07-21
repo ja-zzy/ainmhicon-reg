5ec494d72fedf447586c83eeda92bdd4a326b4cc
ja-zzy <218457306+ja-zzy@users.noreply.github.com>
2025-07-21 17:40:47 +0100

    bump reg open time

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,7 +1,7 @@
 export const CURRENT_CON_ID = 1
 
-// July 21 @ 5pm Irish Time
-const REG_TEST_TIME = 1753120800000
+// July 21 @ 6pm Irish Time
+const REG_TEST_TIME = 1753117200000
 
 // August 4th 2025 @ 7pm Irish Time
 export const REG_START_TIME = REG_TEST_TIME//1754330400000
