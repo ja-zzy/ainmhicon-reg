4b4d4849a5c2c61979cbcb523abb44f39bf37ad7
Ethan Clarke <eclarkelackagh@gmail.com>
2025-08-20 22:34:03 +0100

    Converted stripe control product id into env variable

--- app/api/get-ticket-stock/route.ts
+++ app/api/get-ticket-stock/route.ts
@@ -1,10 +1,12 @@
 import { stripe } from "@/app/utils/private/stripe";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-    try {
-        const controlProduct = await stripe.products.retrieve('prod_StDqDHrmP7fyhx')
+const controlProductId = process.env.STRIPE_CONTROL_PRODUCT_ID
 
+export async function GET() {
+    if (!controlProductId) { throw new Error('Missing Stripe Control Product ID environment variable, ensure STRIPE_CONTROL_PRODUCT_ID is set for this machine') }
+    try {
+        const controlProduct = await stripe.products.retrieve(controlProductId)
         return NextResponse.json(controlProduct.metadata)
     } catch (e) {
         console.error("Cannot get ticket information")
