031d9ed7847837b584c6462d3cdb49e1661e31c6
Ethan Clarke <eclarkelackagh@gmail.com>
2025-08-22 12:43:41 +0100

    Split up capacity logic to ensure it works

--- app/reg/page.tsx
+++ app/reg/page.tsx
@@ -4,8 +4,14 @@ import RegPage from "./pageClient";
 export default async function RegTicketStock() {
     const data = await getTicketStock()
 
-    const saturdayDisabled = parseInt(data.tickets_sold_sat) > parseInt(data.venue_capacity_sat)
-    const sundayDisabled = parseInt(data.tickets_sold_sun) > parseInt(data.venue_capacity_sun)
+    const ticketsSoldSat = parseInt(data.tickets_sold_sat)
+    const venueCapacitySat = parseInt(data.venue_capacity_sat)
+    const saturdayDisabled = ticketsSoldSat >= venueCapacitySat
+
+    const ticketsSoldSun = parseInt(data.tickets_sold_sun)
+    const venueCapacitySun = parseInt(data.venue_capacity_sun)
+    const sundayDisabled = ticketsSoldSun >= venueCapacitySun
+
 
     return (
         <RegPage
