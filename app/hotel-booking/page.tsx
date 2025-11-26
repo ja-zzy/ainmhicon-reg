9180c73534b8c66e3e02a19391b90f06fff8c1ae
Laghairt <48656434+LaghairtDragon@users.noreply.github.com>
2025-11-26 17:06:18 +0000

    Update page.tsx
    Changed wording from "non-flexible" to "flexible" to match how the hotel booking code actually works

--- app/hotel-booking/page.tsx
+++ app/hotel-booking/page.tsx
@@ -15,7 +15,7 @@ export default function HotelBooking() {
             <p>To book a room in our venue hotel, please book directly on their website:</p>
             <p>For more information about staying at the venue please <a href='https://ainmhicon.ie/#pricing' target='_blank' className="link">Click Here</a></p>
             <Link href='https://www.claytonhotels.com/liffey-valley/' target='_blank' className='btn mt-2'>Clayton Hotel Liffey Valley Website</Link>
-            <p>Use the promo code "<b>LOYAL</b>" when completing your booking! This will give a small discount on non-flexible room rates.</p>
+            <p>Use the promo code "<b>LOYAL</b>" when completing your booking! This will give a small discount on flexible room rates.</p>
 
             <button type="button" onClick={navigateBack} className="btn btn-neutral mt-4 w-full">Back to Dashboard</button>
         </>
