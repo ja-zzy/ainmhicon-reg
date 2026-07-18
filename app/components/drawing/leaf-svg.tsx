d352d5b0986a2a7a6f0a0b3f421838d1236bf651
ja-zzy <218457306+ja-zzy@users.noreply.github.com>
2026-07-18 19:22:37 +0100

    crazy business with the leaf designer

---
+++ app/components/drawing/leaf-svg.tsx
@@ -0,0 +1,68 @@
+import { getStroke } from "perfect-freehand";
+import { backgroundColors, Drawing, leafShapes, strokeColors } from "./types";
+
+type Props = {
+  drawing: Drawing;
+};
+
+export function LeafArtwork({
+  drawing,
+}: Props) {
+  const leafPath = leafShapes[drawing.leaf_template];
+  const randomX = 10 + (Math.random() * 80);
+  return (
+    <svg
+      viewBox="0 0 500 600"
+      preserveAspectRatio="xMidYMid meet"
+      className='w-8 transition-[width] duration-150 hover:w-24 absolute'
+      style={{
+        left: `${randomX}%`
+      }}
+    >
+      <defs>
+        <clipPath id="leafClip">
+          <path d={leafPath} />
+        </clipPath>
+      </defs>
+
+      {/* Background */}
+      <path
+        d={leafPath}
+        fill={backgroundColors[drawing.background_color]}
+      />
+
+      {/* Artwork */}
+      <g clipPath="url(#leafClip)">
+        {drawing.strokes.map((stroke, i) => {
+          const outline = getStroke(stroke.points, {
+            size: stroke.size,
+            smoothing: 0.6,
+            streamline: 0.35,
+          });
+
+          const d = outline
+            .map(([x, y], i) =>
+              `${i === 0 ? "M" : "L"} ${x} ${y}`
+            )
+            .join(" ");
+
+          return (
+            <path
+              key={i}
+              d={d}
+              fill={strokeColors[stroke.color]}
+            />
+          );
+        })}
+      </g>
+
+      {/* Outline */}
+      <path
+        d={leafPath}
+        fill="none"
+        stroke="#000"
+        strokeWidth={3}
+      />
+    </svg>
+  );
+}
---
+++ app/leaf-designer/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { DrawingPad } from "../components/drawing/drawing-pad"
+import { Drawing } from "../components/drawing/types"
+import { useAuth } from "../context/authContext"
+
+export default function LeafDesigner() {
+    const authState = useAuth()
+    const [savedDrawing, setSavedDrawing] = useState<Drawing | undefined>()
+    const [fetched, setFetched] = useState(false)
+    const [started, setStarted] = useState(false)
+    useEffect(() => {authState.fetchUsersDrawing().then(d => setSavedDrawing(d)).finally(() => setFetched(true))}, [authState])
+    return (
+        <div className="w-full md:w-[80%] m-auto py-16">
+            <div className={`flex flex-col gap-4 ${started && 'hidden'}`}>
+                <h1 className="font-[family-name:var(--font-sora)] text-xl">Design a Leaf!</h1>
+                <p>Help us spread our roots and grow! As a thank you for registering and showing your support we'd like to invite you to
+                    design your very own leaf which will appear on the website and in various places around the convention.
+                    <br /><br />
+                    Please <b>keep it SFW!</b> Leaves are associated with your account and will be removed if innappropriate.</p>
+                <button className='btn rounded-lg transition-all duration-150 hover:brightness-125' onClick={() => setStarted(true)}>Get started</button>
+            </div>
+           {started && fetched && <DrawingPad drawing={savedDrawing} onSave={(d) => authState.updateLeaf(d)}/>}
+       </div>
+    )
+}
